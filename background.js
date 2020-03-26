7654ef780871edd7f997057d47424a3ec077d02f
christianhubbard <59040087+christianhubbard@users.noreply.github.com>
2020-03-26 09:30:28 -0700

    with framework added

--- background.js
+++ background.js
@@ -1,34 +1,33 @@
 const checkMovement = () => {
-    const req = new XMLHttpRequest()
-      
-        req.onreadystatechange = () => {
-          if(req.readyState === 4 && req.status === 200) {
-            alertMovement(JSON.parse(req.responseText))
-          }
-        }
-      
-        // req.open('GET', 'https://api.coinranking.com/v1/public/coins/?limit=10')
-        req.open('GET', 'https://api.coincap.io/v2/assets/?limit=10')
-         
-        //maybe need to come back an specify content type
-        req.setRequestHeader('Accept-Language', "en-US")
-      
-        req.send()
-}
+  const req = new XMLHttpRequest();
 
-const alertMovement = (data) => {
-    data.data.forEach(coin => {
-   
-    if(coin.priceUsd / coin.vwap24Hr > 1.01) {
-        alert(`${coin.name} PRICE MOVEMENT`)
-    } else if(coin.priceUsd / coin.vwap24Hr < .99) {
-        alert(`${coin.name} PRICE MOVEMENT`)
+  req.onreadystatechange = () => {
+    if (req.readyState === 4 && req.status === 200) {
+      alertMovement(JSON.parse(req.responseText));
     }
-    })
-    //if price movement is x amount
-     //window alert
-    //must figure out how to recursivley fetch data 
-    // setTimeout(checkMovement, 60000)
-}
+  };
 
-checkMovement()
+  // req.open('GET', 'https://api.coinranking.com/v1/public/coins/?limit=10')
+  req.open('GET', 'https://api.coincap.io/v2/assets/?limit=10');
+
+  //maybe need to come back an specify content type
+  req.setRequestHeader('Accept-Language', 'en-US');
+
+  req.send();
+};
+
+const alertMovement = data => {
+  data.data.forEach(coin => {
+    if (coin.priceUsd / coin.vwap24Hr > 1.01) {
+      // alert(`${coin.name} PRICE MOVEMENT`)
+    } else if (coin.priceUsd / coin.vwap24Hr < 0.99) {
+      // alert(`${coin.name} PRICE MOVEMENT`)
+    }
+  });
+  //if price movement is x amount
+  //window alert
+  //must figure out how to recursivley fetch data
+  // setTimeout(checkMovement, 60000)
+};
+
+checkMovement();
--- main.js
+++ main.js
@@ -1,43 +1,44 @@
-//render top 10 crypto tickers to popup 
+//render top 10 crypto tickers to popup
 
 //create some functionality to alert user when a big price move has happened
-    //while data is coming in form api checking for movement
-    //if big move happens, send an alert
+//while data is coming in form api checking for movement
+//if big move happens, send an alert
 
-    const loadCoins = () => {
-        const req = new XMLHttpRequest()
-      
-        req.onreadystatechange = () => {
-          if(req.readyState === 4 && req.status === 200) {
-            renderCoins(JSON.parse(req.responseText))
-          }
-        }
-      
-        // req.open('GET', 'https://api.coinranking.com/v1/public/coins/?limit=10')
-        req.open('GET', 'https://api.coincap.io/v2/assets/?limit=10')
-         
-        //maybe need to come back an specify content type
-        req.setRequestHeader('Accept-Language', "en-US")
-      
-        req.send()
-      }
-    
-      //data.coins[i].name
-    const renderCoins = (data) => {
-        const content = document.getElementById('content')
-        console.log(data)
-        
-        data.data.forEach(coin=> {
-            const main = document.createElement('div')
-            main.className = 'coinDiv'
-            main.innerHTML = `
-                <div>${coin.rank}: ${coin.name}</div>
-                <div>${coin.priceUsd}</div>
-            ` 
+const loadCoins = () => {
+  const req = new XMLHttpRequest();
 
-           content.appendChild(main)
-        })
+  req.onreadystatechange = () => {
+    if (req.readyState === 4 && req.status === 200) {
+      renderCoins(JSON.parse(req.responseText));
+    }
+  };
 
-    } 
+  // req.open('GET', 'https://api.coinranking.com/v1/public/coins/?limit=10')
+  req.open('GET', 'https://api.coincap.io/v2/assets/?limit=10');
 
-    loadCoins()
+  //maybe need to come back an specify content type
+  req.setRequestHeader('Accept-Language', 'en-US');
+
+  req.send();
+};
+
+//data.coins[i].name
+const renderCoins = data => {
+  const content = document.getElementById('content');
+  console.log(data);
+
+  data.data.forEach(coin => {
+    const main = document.createElement('div');
+    const price = coin.priceUsd;
+    const roundedPrice = Math.round(price * 100) / 100;
+    main.className = 'coinDiv';
+    main.innerHTML = `
+                <div class = "columns"><div class="notification"><div class= "column" align="left">${coin.rank}: ${coin.name}</div>
+                <div class = "column" align="right">${roundedPrice}</div></div></div>
+            `;
+
+    content.appendChild(main);
+  });
+};
+
+loadCoins();
